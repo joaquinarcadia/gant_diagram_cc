70e157dc2589742f5c4f9f38bfca62cd72f29837
joaquin <villanuevajoaquin94@gmail.com.com>
2025-08-20 15:47:15 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/gant_diagram_cc",
+    base: "/gant_diagram_cc/",
     plugins: [react()],
     preview: {
         port: 8080,
