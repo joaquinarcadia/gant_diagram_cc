0ddba97d9dae4e8c4f8d8ff18e1d9420f220baa4
joaquinarcadia <155553409+joaquinarcadia@users.noreply.github.com>
2025-08-21 11:33:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/gant_diagram_cc/",
+    base: "/",
     plugins: [react()],
     preview: {
         port: 8080,
